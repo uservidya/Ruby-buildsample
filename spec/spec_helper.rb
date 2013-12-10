53c6ca680c50f4ca00f62257b59b20547b03653c
Vidya3 <vidya_test1@yahoo.in>
2013-12-10 12:44:03 +0530

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'rcov'
 require 'rcov-csv'
-rcov.formatter = RCov::Formatter::CSVFormatter
+rcov.formatter = rcov::Formatter::CSVFormatter
 rcov.coverage_dir(ENV["COVERAGE_REPORTS"])
 rcov.start
